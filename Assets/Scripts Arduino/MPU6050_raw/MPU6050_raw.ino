5544c4fd34d4b0e1ac07d3f2795c12232762955c
Jonatan <al315616@uji.es>
2017-09-27 18:06:54 +0200

    Arduino Go Go Go!!
    Pues esto ya va

--- Assets/Scripts Arduino/MPU6050_raw/MPU6050_raw.ino
+++ Assets/Scripts Arduino/MPU6050_raw/MPU6050_raw.ino
@@ -58,7 +58,6 @@ int16_t ax, ay, az;
 int16_t gx, gy, gz;
 
 
-
 // uncomment "OUTPUT_READABLE_ACCELGYRO" if you want to see a tab-separated
 // list of the accel X/Y/Z and then gyro X/Y/Z values in decimal. Easy to read,
 // not so easy to parse, and slow(er) over UART.
@@ -85,7 +84,7 @@ void setup() {
     // initialize serial communication
     // (38400 chosen because it works as well at 8MHz as it does at 16MHz, but
     // it's really up to you depending on your project)
-    Serial.begin(9600);
+    Serial.begin(38400);
 
     // initialize device
     Serial.println("Initializing I2C devices...");
@@ -96,8 +95,9 @@ void setup() {
     Serial.println(accelgyro.testConnection() ? "MPU6050 connection successful" : "MPU6050 connection failed");
 
     // use the code below to change accel/gyro offset values
-    /*
-    Serial.println("Updating internal sensor offsets...");
+
+    //ESTABA COMENTADO
+    /*Serial.println("Updating internal sensor offsets...");
     // -76	-2359	1688	0	0	0
     Serial.print(accelgyro.getXAccelOffset()); Serial.print("\t"); // -76
     Serial.print(accelgyro.getYAccelOffset()); Serial.print("\t"); // -2359
@@ -115,8 +115,8 @@ void setup() {
     Serial.print(accelgyro.getXGyroOffset()); Serial.print("\t"); // 0
     Serial.print(accelgyro.getYGyroOffset()); Serial.print("\t"); // 0
     Serial.print(accelgyro.getZGyroOffset()); Serial.print("\t"); // 0
-    Serial.print("\n");
-    */
+    Serial.print("\n");*/
+    
 
     // configure Arduino LED for
     pinMode(LED_PIN, OUTPUT);
@@ -129,6 +129,8 @@ void setup() {
 }
 
 void loop() {
+
+    Serial.flush();
     // read raw accel/gyro measurements from device
     accelgyro.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
 
@@ -138,20 +140,23 @@ void loop() {
 
     #ifdef OUTPUT_READABLE_ACCELGYRO
         // display tab-separated accel/gyro x/y/z values
-        Serial.print("a/g:\t");
-        Serial.println(ax - 8300); //Serial.print("\t");
-        Serial.print(ay); Serial.print("\t");
-        Serial.print(az); Serial.print("\t");
-        Serial.print(gx); Serial.print("\t");
-        Serial.print(gy); Serial.print("\t");
+        
+        Serial.print(ax); Serial.print(",");
+        Serial.print(ay); Serial.print(",");
+        Serial.print(az); Serial.print(",");
+        Serial.print(gx); Serial.print(",");
+        Serial.print(gy); Serial.print(",");
         Serial.println(gz);
 
+      /*
         if (Serial.available() > 0)
             sCmd.readSerial();
 
         
         delay(500);
+        */
     #endif
+
 
     #ifdef OUTPUT_BINARY_ACCELGYRO
         Serial.write((uint8_t)(ax >> 8)); Serial.write((uint8_t)(ax & 0xFF));
@@ -165,11 +170,12 @@ void loop() {
     // blink LED to indicate activity
     blinkState = !blinkState;
     digitalWrite(LED_PIN, blinkState);
+    delay(100);
 }
 
 
-void pingHandler ()
-{
+void pingHandler () //No se le llama
+{  
   Serial.println(ax);
 }
 
