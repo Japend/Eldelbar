6fef1f7712e8020c075cbbd129a39121f555be4b
kikoalfaro <kikoalfaro2@gmail.com>
2017-09-27 18:33:17 +0200

    Ángulos acelerómetro

--- Assets/Scripts Arduino/MPU6050_raw/MPU6050_raw.ino
+++ Assets/Scripts Arduino/MPU6050_raw/MPU6050_raw.ino
@@ -36,10 +36,9 @@ THE SOFTWARE.
 #include "I2Cdev.h"
 #include "MPU6050.h"
 
-#include <SerialCommand.h>
 #include <SoftwareSerial.h>
 
-SerialCommand sCmd;
+
 
 // Arduino Wire library is required if I2Cdev I2CDEV_ARDUINO_WIRE implementation
 // is used in I2Cdev.h
@@ -58,21 +57,21 @@ int16_t ax, ay, az;
 int16_t gx, gy, gz;
 
 
-
 // uncomment "OUTPUT_READABLE_ACCELGYRO" if you want to see a tab-separated
 // list of the accel X/Y/Z and then gyro X/Y/Z values in decimal. Easy to read,
 // not so easy to parse, and slow(er) over UART.
 #define OUTPUT_READABLE_ACCELGYRO
 
-// uncomment "OUTPUT_BINARY_ACCELGYRO" to send all 6 axes of data as 16-bit
-// binary, one right after the other. This is very fast (as fast as possible
-// without compression or data loss), and easy to parse, but impossible to read
-// for a human.
-//#define OUTPUT_BINARY_ACCELGYRO
-
 
 #define LED_PIN 13
 bool blinkState = false;
+
+
+//variables para calcular orientacion a partir de datos del giroscopio
+long tiempo_prev, dt;
+float girosc_ang_z, girosc_ang_y;
+float girosc_ang_z_prev, girosc_ang_y_prev;
+
 
 void setup() {
     // join I2C bus (I2Cdev library doesn't do this automatically)
@@ -85,7 +84,7 @@ void setup() {
     // initialize serial communication
     // (38400 chosen because it works as well at 8MHz as it does at 16MHz, but
     // it's really up to you depending on your project)
-    Serial.begin(9600);
+    Serial.begin(38400);
 
     // initialize device
     Serial.println("Initializing I2C devices...");
@@ -96,39 +95,16 @@ void setup() {
     Serial.println(accelgyro.testConnection() ? "MPU6050 connection successful" : "MPU6050 connection failed");
 
     // use the code below to change accel/gyro offset values
-    /*
-    Serial.println("Updating internal sensor offsets...");
-    // -76	-2359	1688	0	0	0
-    Serial.print(accelgyro.getXAccelOffset()); Serial.print("\t"); // -76
-    Serial.print(accelgyro.getYAccelOffset()); Serial.print("\t"); // -2359
-    Serial.print(accelgyro.getZAccelOffset()); Serial.print("\t"); // 1688
-    Serial.print(accelgyro.getXGyroOffset()); Serial.print("\t"); // 0
-    Serial.print(accelgyro.getYGyroOffset()); Serial.print("\t"); // 0
-    Serial.print(accelgyro.getZGyroOffset()); Serial.print("\t"); // 0
-    Serial.print("\n");
-    accelgyro.setXGyroOffset(220);
-    accelgyro.setYGyroOffset(76);
-    accelgyro.setZGyroOffset(-85);
-    Serial.print(accelgyro.getXAccelOffset()); Serial.print("\t"); // -76
-    Serial.print(accelgyro.getYAccelOffset()); Serial.print("\t"); // -2359
-    Serial.print(accelgyro.getZAccelOffset()); Serial.print("\t"); // 1688
-    Serial.print(accelgyro.getXGyroOffset()); Serial.print("\t"); // 0
-    Serial.print(accelgyro.getYGyroOffset()); Serial.print("\t"); // 0
-    Serial.print(accelgyro.getZGyroOffset()); Serial.print("\t"); // 0
-    Serial.print("\n");
-    */
 
+ 
     // configure Arduino LED for
     pinMode(LED_PIN, OUTPUT);
 
-    while (!Serial);
-
-    sCmd.addCommand("PING", pingHandler);
-    sCmd.addCommand("ECHO", echoHandler);
-    
 }
 
 void loop() {
+
+    Serial.flush();
     // read raw accel/gyro measurements from device
     accelgyro.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
 
@@ -136,50 +112,35 @@ void loop() {
     //accelgyro.getAcceleration(&ax, &ay, &az);
     //accelgyro.getRotation(&gx, &gy, &gz);
 
+    updateGiro();
     #ifdef OUTPUT_READABLE_ACCELGYRO
         // display tab-separated accel/gyro x/y/z values
-        Serial.print("a/g:\t");
-        Serial.println(ax - 8300); //Serial.print("\t");
-        Serial.print(ay); Serial.print("\t");
-        Serial.print(az); Serial.print("\t");
-        Serial.print(gx); Serial.print("\t");
-        Serial.print(gy); Serial.print("\t");
-        Serial.println(gz);
-
-        if (Serial.available() > 0)
-            sCmd.readSerial();
-
         
-        delay(500);
+        Serial.print(ax); Serial.print(",");
+        Serial.print(ay); Serial.print(",");
+        Serial.print(az); Serial.print(",");
+        Serial.print(girosc_ang_y); Serial.print(",");
+        Serial.println(girosc_ang_z); //Serial.print(",");
+        //Serial.println(gz);
+
     #endif
 
-    #ifdef OUTPUT_BINARY_ACCELGYRO
-        Serial.write((uint8_t)(ax >> 8)); Serial.write((uint8_t)(ax & 0xFF));
-        Serial.write((uint8_t)(ay >> 8)); Serial.write((uint8_t)(ay & 0xFF));
-        Serial.write((uint8_t)(az >> 8)); Serial.write((uint8_t)(az & 0xFF));
-        Serial.write((uint8_t)(gx >> 8)); Serial.write((uint8_t)(gx & 0xFF));
-        Serial.write((uint8_t)(gy >> 8)); Serial.write((uint8_t)(gy & 0xFF));
-        Serial.write((uint8_t)(gz >> 8)); Serial.write((uint8_t)(gz & 0xFF));
-    #endif
 
     // blink LED to indicate activity
     blinkState = !blinkState;
     digitalWrite(LED_PIN, blinkState);
+    delay(100);
 }
 
-
-void pingHandler ()
+void updateGiro()
 {
-  Serial.println(ax);
-}
-
-void echoHandler ()
-{
-  char *arg;
-  arg = sCmd.next();
-  if (arg != NULL)
-    Serial.println(arg);
-  else
-    Serial.println("nothing to echo");
+   dt = millis() - tiempo_prev;
+   tiempo_prev = millis();
+ 
+   girosc_ang_z = (gz / 131)*dt / 1000.0 + girosc_ang_z_prev;
+   girosc_ang_y = (gy / 400)*dt / 1000.0 + girosc_ang_y_prev;
+ 
+   girosc_ang_z_prev = girosc_ang_z;
+   girosc_ang_y_prev = girosc_ang_y;
 }
 
