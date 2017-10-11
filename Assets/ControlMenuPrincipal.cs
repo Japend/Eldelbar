f9a2767c84fa4ae3697285aecd0358ea2baa769a
Jonatan <al315616@uji.es>
2017-10-11 17:29:46 +0200

    Clase Primera Mitad End

---
+++ Assets/ControlMenuPrincipal.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlMenuPrincipal : MonoBehaviour {
+
+	private GameObject continuar;
+	private GameObject empezar;
+	private GameObject salir;
+	private GameObject calibrar;
+	private GameObject calibrando;
+
+	int estadoAnterior;
+
+	void Awake () {
+		continuar = GameObject.Find ("Continuar");
+		empezar = GameObject.Find ("Empezar");
+		calibrar = GameObject.Find ("Calibrar");
+		salir = GameObject.Find ("Salir");
+		calibrando = GameObject.Find ("Calibrando");
+		estadoAnterior = -1;  //para que se ejecute el switch la primera vez
+	}
+		
+
+	void Update () {
+
+		if (estadoAnterior != GlobalData.EstadoJuego) {
+			estadoAnterior = GlobalData.EstadoJuego;
+
+			switch (GlobalData.EstadoJuego) {
+
+			case GlobalData.MENU_PRINCIPAL:
+				empezar.SetActive (true);
+				salir.SetActive (true);
+				break;
+
+			}
+		}
+	}
+}
--- Assets/Scripts/CubeMovement.cs
+++ Assets/Scripts/CubeMovement.cs
@@ -9,14 +9,15 @@ public class CubeMovement : MonoBehaviour {
     private float rotationX;
     private float rotationY;
     private float accelX;
-    private float accelY;
+   //private float accelY;
     private float accelZ;
-    public float speed = 20f;
+    public float speed = 1f;
+    private float accelation = 0.5f;
     private Vector3 accel;
     private float[] rotXDatos = new float[5] { 0,0,0,0,0};
     private float[] rotYDatos = new float[5] { 0, 0, 0, 0, 0 };
     private float[] accXDatos = new float[5] { 0, 0, 0, 0, 0 };
-    private float[] accYDatos = new float[5] { 0, 0, 0, 0, 0 };
+    //private float[] accYDatos = new float[5] { 0, 0, 0, 0, 0 };
     private float[] accZDatos = new float[5] { 0, 0, 0, 0, 0 };
     int ciclo = 0;
 
@@ -119,19 +120,15 @@ public class CubeMovement : MonoBehaviour {
         rotationX = float.Parse(rotX);
         rotationY = float.Parse(rotY);
         accelX = float.Parse(accX);
-        accelY = float.Parse(accY);
-        accelZ = float.Parse(accZ)/1.5f;
-
-        //accel = new Vector3(accelX, accelY, accelZ);
-        //accel = new Vector3(0, -accelZ, 0);
-        //accel = new Vector3(-accelX, 0, 0);
-        accel = new Vector3(accelX, -accelZ, accelY);
-
+        //accelY = float.Parse(accY);
+        accelZ = float.Parse(accZ);
+        Debug.Log(accelZ);
+        DeleteRuido();
+        Ajuste(rotationX,rotationY,accelX,  0,accelZ); 
+        //accel = new Vector3(accelX, 0, 0);
         player.transform.localEulerAngles = new Vector3(rotationX, 0,rotationY);
-        //player.transform.localEulerAngles = new Vector3(0, 0, rotationY);
-        //player.transform.position = new Vector3(accelX,accelY, accelZ);
-        //player.transform.position = new Vector3(accelX, accelY, accelZ) + player.transform.localPosition;
-        player.transform.Translate(speed * accel * Time.deltaTime );
+        //player.transform.Translate(accel);
+
 
 
     }
@@ -142,14 +139,93 @@ public class CubeMovement : MonoBehaviour {
         rotXDatos[ciclo] = rotationX;
         rotYDatos[ciclo] = rotationY;
         accXDatos[ciclo] = accelX;
-        accYDatos[ciclo] = accelY;
         accZDatos[ciclo] = accelZ;
 
         ciclo++;
         if (ciclo > 4) {
             ciclo = 0;
         }
+        Mezclar();
+    }
 
+    private void Mezclar() {
+
+        /*float counterRotX = 0f;
+        float counterRotY = 0f;
+        float counterAccelX = 0f;
+        float counterAccelZ = 0f;
+
+
+
+        for (int i = 0; i < 5; i++) {
+
+            counterRotX = counterRotX + rotXDatos[i];
+            counterRotY = counterRotY + rotYDatos[i];
+            counterAccelX = counterAccelX + accXDatos[i];
+            counterAccelZ = counterAccelZ + accZDatos[i];
+
+        }
+
+        rotationX = counterRotX / 5f;
+        rotationY = counterRotY / 5f;
+        accelX = accelX / 5f;
+        accelZ = accelZ / 5f;*/
+        //Ajuste
+        int ant;
+        if (ciclo == 0)
+        {
+            ant = 4;
+        }
+        else {
+
+            ant = ciclo - 1;
+        }
+        //Aplicar Velocidad segun la dirección
+        if (accXDatos[ciclo] > accXDatos[ant] + 0.6f)
+        {
+
+            player.transform.Translate(2,0,0);
+            
+
+        }
+        else if (accXDatos[ciclo] < accXDatos[ant]-0.6f)
+        {
+
+            player.transform.Translate(-2, 0, 0);
+
+
+        }
+
+        if (accZDatos[ciclo] > accZDatos[ant] + 0.6f)
+        {
+
+            player.transform.Translate(0, -2, 0);
+
+        }
+        else if (accZDatos[ciclo] < accZDatos[ant] - 0.6f)
+        {
+
+            player.transform.Translate(0, 2, 0);
+
+
+        }
+
+
+    }
+
+    private void DeleteRuido() {
+
+        if (accelX > -0.15f && accelX < 0.15f) {
+
+            accelX = 0f;
+        }
+
+
+        if (accelZ > -0.15f && accelZ < 0.15f)
+        {
+
+            accelZ = 0f;
+        }
 
     }
 }
