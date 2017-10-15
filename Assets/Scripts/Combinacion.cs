cc3e67cc5f5372c07e04909bde9983de6605c775
kikoalfaro <kikoalfaro2@gmail.com>
2017-10-15 17:33:46 +0200

    Añadidos scripts combinación y ultrasonidos

---
+++ Assets/Scripts/Combinacion.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Combinacion : MonoBehaviour {
+
+    Text text;
+    public int pin;
+
+	// Use this for initialization
+	void Start () {
+        text = GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        text.color = (CombinacionManager.GetPinesActivos()[pin]) ? Color.red : Color.black;
+	}
+}
