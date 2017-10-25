fdb0c98dee7e3aae4709c7537b8a24e5d449ff2e
kikoalfaro <kikoalfaro2@gmail.com>
2017-10-25 02:04:06 +0200

    Efectos visuales molones :D

---
+++ Assets/efectoGiro.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class efectoGiro : MonoBehaviour {
+
+public float turnSpeed;
+public char eje;
+Vector3 vectorGiro;
+	// Use this for initialization
+	void Start () {
+		switch(eje){
+			case 'x':
+			vectorGiro = Vector3.right;
+			break;
+			case 'y':
+			vectorGiro = Vector3.up;
+			break;
+			case 'z':
+			vectorGiro = Vector3.forward;
+			break;
+			case 'a':
+			vectorGiro = new Vector3(1, 1, 1);
+			break;
+
+
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(vectorGiro, turnSpeed * Time.deltaTime);
+	}
+}
